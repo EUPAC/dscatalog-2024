23afe8b5607f01fda5a076b99d3c470d151a75dd
Eugênio Pacceli Costa <eugenio@sisger.com.br>
2024-03-14 18:41:05 -0300

    Backend project created

---
+++ backend/src/main/java/com/sisger/DSCatalog24/DsCatalog24Application.java
@@ -0,0 +1,13 @@
+package com.sisger.DSCatalog24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalog24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalog24Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sisger/DSCatalog24/DsCatalog24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisger.DSCatalog24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalog24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
