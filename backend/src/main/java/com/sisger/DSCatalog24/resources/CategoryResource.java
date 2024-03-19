c5c2bf27e012bf96425b8df699f5b00328a5ee58
Eugênio Pacceli Costa <eugenio@sisger.com.br>
2024-03-18 22:24:15 -0300

    DTO

--- backend/src/main/java/com/sisger/DSCatalog24/resources/CategoryResource.java
+++ backend/src/main/java/com/sisger/DSCatalog24/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sisger.DSCatalog24.entities.Category;
+import com.sisger.DSCatalog24.dto.CategoryDTO;
 import com.sisger.DSCatalog24.services.CategoryService;
 
 @RestController
@@ -16,12 +16,11 @@ import com.sisger.DSCatalog24.services.CategoryService;
 public class CategoryResource {
 	
 	@Autowired
-	private CategoryService service;
+	private CategoryService service; 
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll (){
-		List<Category> list = service.findAll();
-		
+	public ResponseEntity<List<CategoryDTO>> findAll (){
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
