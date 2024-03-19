f2f5a3f929bb4779e87ee2ca8d6e85537e9a97b2
Eugênio Pacceli Costa <eugenio@sisger.com.br>
2024-03-19 10:46:00 -0300

    Tratamento de exceções

--- backend/src/main/java/com/sisger/DSCatalog24/services/CategoryService.java
+++ backend/src/main/java/com/sisger/DSCatalog24/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.sisger.DSCatalog24.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sisger.DSCatalog24.dto.CategoryDTO;
 import com.sisger.DSCatalog24.entities.Category;
 import com.sisger.DSCatalog24.repositories.CategoryRepository;
+import com.sisger.DSCatalog24.services.exceptions.EntityNotFoundException;
+
+
 
 @Service
 public class CategoryService {
@@ -23,6 +27,13 @@ public class CategoryService {
 		
 		return list.stream().map(x->new CategoryDTO(x)).collect(Collectors.toList());
  	
+	}
+
+	@Transactional(readOnly = true)  
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 
 }
